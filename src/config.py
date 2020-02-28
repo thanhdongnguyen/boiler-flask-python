b99f732a3d9e09b71bd2068f515b6de9fa49ac95
NguyenThanhDong <nguyenthanhdong222@gmail.com>
2020-02-28 11:49:05 +0700

    change name file logs and remove pid

--- src/config.py
+++ src/config.py
@@ -9,7 +9,7 @@ import os
 
 load_dotenv(dotenv_path="/src/.env")
 date = Timer.get_current_format()
-file_name = "{}/flask_teamate_{}.log".format("/src/logs", date)
+file_name = "{}/flask_log_{}.log".format("/src/logs", date)
 
 
 HOST = os.getenv("HOST")
@@ -29,7 +29,6 @@ spew = False
 reload = True
 
 daemon=False
-#pidfile=/tmp/hackathon.pid
 umask=0
 user=None
 group=None
